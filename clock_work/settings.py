da8cd8ba83fcfc3bd6f69ef1b1dffab473467464
arpansahu <arpanrocks95@gmail.com>
2024-06-02 19:42:00 +0530

    updated back with old staticfile settings

--- clock_work/settings.py
+++ clock_work/settings.py
@@ -140,35 +140,21 @@ USE_TZ = True
 if not DEBUG:
     BUCKET_TYPE = config('BUCKET_TYPE')
 
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-
     if BUCKET_TYPE == 'AWS':
 
+        AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+        AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
         AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-    elif BUCKET_TYPE == 'BLACK_BLAZE':
-        AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
-        AWS_S3_ENDPOINT = f's3.{AWS_S3_REGION_NAME}.backblazeb2.com'
-        AWS_S3_ENDPOINT_URL = f'https://{AWS_S3_ENDPOINT}'
-
-    elif BUCKET_TYPE == 'MINIO':
-        AWS_S3_ENDPOINT_URL = f'https://arpansahu.me:9002'
-        # AWS_S3_USE_SSL=False
-
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400'
-    }
-
-    AWS_LOCATION = 'static'
-    AWS_QUERYSTRING_AUTH = False
-    AWS_HEADERS = {
-        'Access-Control-Allow-Origin': '*',
-    }
-    if BUCKET_TYPE == 'AWS':
-
+        AWS_DEFAULT_ACL = 'public-read'
+        AWS_S3_OBJECT_PARAMETERS = {
+            'CacheControl': 'max-age=86400'
+        }
+        AWS_LOCATION = 'static'
+        AWS_QUERYSTRING_AUTH = False
+        AWS_HEADERS = {
+            'Access-Control-Allow-Origin': '*',
+        }
         # s3 static settings
         AWS_STATIC_LOCATION = 'portfolio/clock_work/static'
         STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
@@ -180,7 +166,27 @@ if not DEBUG:
         # s3 private media settings
         PRIVATE_MEDIA_LOCATION = 'portfolio/clock_work/private'
         PRIVATE_FILE_STORAGE = 'clock_work.storage_backends.PrivateMediaStorage'
-    else:
+
+    elif BUCKET_TYPE == 'BLACKBLAZE':
+
+        AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+        AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+        AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+
+        AWS_S3_ENDPOINT = f's3.{AWS_S3_REGION_NAME}.backblazeb2.com'
+        AWS_S3_ENDPOINT_URL = f'https://{AWS_S3_ENDPOINT}'
+        
+        AWS_DEFAULT_ACL = 'public-read'
+        AWS_S3_OBJECT_PARAMETERS = {
+            'CacheControl': 'max-age=86400',
+        }
+
+        AWS_LOCATION = 'static'
+        AWS_QUERYSTRING_AUTH = False
+        AWS_HEADERS = {
+            'Access-Control-Allow-Origin': '*',
+        }
         # s3 static settings
         AWS_STATIC_LOCATION = 'portfolio/clock_work/static'
         STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_STATIC_LOCATION}/'
